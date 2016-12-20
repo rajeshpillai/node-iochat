a892ec3ecc49041a4fd32a101870b56ea59ae409
Rajesh Pillai <pillai.rajesh@gmail.com>
2016-12-20 14:00:23 +0530

    03-basic socket client and server setup

--- server.js
+++ server.js
@@ -7,16 +7,33 @@ var io = require("socket.io").listen(server);
 users = [];
 connections = [];
 
-server.listen(process.env.PORT || 9999, function (){
-    console.log("Server running on 9999");
+var port = process.env.PORT || 9999;
+
+server.listen(port, function (){
+    console.log(`Server running on {{port}}`);
 });
 
 app.get("/", function (req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", function (socket){
+io.sockets.on("connection", function (socket){
     connections.push(socket);
     console.log("Connected: %s sockets connected", connections.length);
+
+
+    socket.on("disconnect", function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected: %s sockets connected", connections.length);
+    })
+
+    socket.on("send message", function (data) {
+        console.log(data);
+        io.sockets.emit("new message", {msg: data});
+    })
 });
+
+
+
+
 
