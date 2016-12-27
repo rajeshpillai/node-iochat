e0fcc31cd40ca03c411197b211b36202b316535b
Rajesh Pillai <pillai.rajesh@gmail.com>
2016-12-27 07:12:44 +0530

    Logged port no on console

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connections = [];
 var port = process.env.PORT || 9999;
 
 server.listen(port, function (){
-    console.log(`Server running on {{port}}`);
+    console.log(`Server running on ${port}`);
 });
 
 app.get("/", function (req, res){
